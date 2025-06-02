2b558b0768a42c3913d99aedc104633560d519be
KevinMI <kevin.bevenhall@medieinstitutet.se>
2025-06-02 16:19:41 +0200

    NextAuth, Login, Register

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,59 @@
+import { connectDB } from "@/lib/mongoose";
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import User, {IUser} from "@/models/User";
+import bcrypt from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials) {
+      await connectDB();
+
+      if (!credentials?.email || !credentials.password) return null;
+
+      const user = await User.findOne({ email: credentials.email });
+      if (!user) return null;
+
+      const isValid = await bcrypt.compare(credentials.password, user.password);
+      if (!isValid) return null;
+
+      return {
+        id: user._id.toString(),
+        email: user.email,
+        subscriptionLevel: user.subscriptionLevel
+      }
+    }
+    })
+  ],
+
+  session: {
+    strategy: "jwt"
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.subscriptionLevel = user.subscriptionLevel;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.subscriptionLevel = token.subscriptionLevel as "Explorer" | "Odyssey" | "Mastermind";
+      }
+      return session;
+    }
+  }
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
