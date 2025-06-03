e4e003e989dba3772e58dd8cd018498f303343ff
Vera <v.bergstrom83@yahoo.se>
2025-06-03 22:41:44 +0200

    add dependencies route and component for subscriptions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
-import SessionProviderWrapper from "./components/SessionProviderWrapper";
+import SessionProviderWrapper from "../components/SessionProviderWrapper";
+import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-        <SessionProviderWrapper>
-          {children}
-        </SessionProviderWrapper>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
   );
