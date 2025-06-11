80dcb2ab20ca63768913036ae82e65b696517194
Vera <v.bergstrom83@yahoo.se>
2025-06-11 15:44:52 +0200

    working stripe flow

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, {Document, Model, Schema} from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 
 export interface IUser extends Document {
   _id: mongoose.Types.ObjectId;
@@ -6,15 +6,53 @@ export interface IUser extends Document {
   password: string;
   subscriptionLevel: "free" | "basic" | "pro" | "premium";
   role: "customer" | "admin";
+  subscriptionStatus?: "active" | "canceled" | "past_due" | "unpaid";
+  subscriptionStartDate?: Date; // Nytt fält för startdatumet
+  subscriptionEndDate?: Date;
+  stripeCustomerId?: string;
+  stripeSubscriptionId?: string;
+  stripeDetails?: {
+    // Nytt fält för detaljerad Stripe-information
+    status?: string;
+    cancelAtPeriodEnd?: boolean;
+    currentPeriodStart?: Date;
+    currentPeriodEnd?: Date;
+  };
 }
 
 const UserSchema: Schema<IUser> = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  subscriptionLevel: { type: String, enum: ["free", "basic", "pro", "premium",], default: "free", required: true },
-  role: { type: String, enum: ["customer", "admin"], default: "customer", required: true }
-})
+  subscriptionLevel: {
+    type: String,
+    enum: ["free", "basic", "pro", "premium"],
+    default: "free",
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "admin"],
+    default: "customer",
+    required: true,
+  },
+  subscriptionStatus: {
+    type: String,
+    enum: ["active", "canceled", "past_due", "unpaid"],
+  },
+  subscriptionStartDate: { type: Date }, // Nytt fält
+  subscriptionEndDate: { type: Date },
+  stripeCustomerId: { type: String },
+  stripeSubscriptionId: { type: String },
+  stripeDetails: {
+    // Nytt fält för att lagra mer detaljerad information från Stripe
+    status: { type: String },
+    cancelAtPeriodEnd: { type: Boolean },
+    currentPeriodStart: { type: Date },
+    currentPeriodEnd: { type: Date },
+  },
+});
 
-const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
 
 export default User;
